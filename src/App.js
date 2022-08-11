ca165af10e9083b372310bad6153894a1fdfdaad
bt-ausbildung <ng-ausbildung@gmail.com>
2022-08-11 14:46:17 +0200

    commit 1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
+import Navigation from "./components/navigation/Navigation";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // fetch posts
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => response.json())
+      .then((dataPosts) => setPosts(dataPosts));
+  }, []);
+
+  const slide = () => {};
+
+  return (
+      <div className="App">
+        <div className="components">
+        <Navigation />
+        <Content posts={posts} />
+        <Footer />
+        </div>
+      </div>
+  );
+}
+
+export default App;
